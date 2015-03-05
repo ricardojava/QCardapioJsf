b8513c35b9a3de83877ac21ddb9943922da762a3
RicardoJava <ricardobatistaalmeida@gmail.com>
2015-03-04 23:25:41 -0300

    commit hello

---
+++ QCardapioJsf/src/main/java/br/com/calltek/qcardapio/view/Hello.java
@@ -0,0 +1,23 @@
+package br.com.calltek.qcardapio.view;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean
+public class Hello {
+	
+	@PostConstruct
+	public void init(){
+		System.out.println(" Bean executado! ");
+	}
+	
+	public String getMessage(){
+		return "Hello World JSF!";
+	}
+
+}
+
+
+
